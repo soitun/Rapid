de9b016594c839e030acdfef1246058c367c163e
Gareth Edwards <gareth.edwards@rapid-is.co.uk>
2014-09-26 10:10:26 +0100

    un-versioned applications now have all links updated when files are moved to versioned location

--- src/com/rapid/utils/Strings.java
+++ src/com/rapid/utils/Strings.java
@@ -1,5 +1,6 @@
 package com.rapid.utils;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
@@ -9,6 +10,17 @@ public class Strings {
 	public static String getString(InputStream is) throws IOException {
 		// prepare a scanner
         Scanner s = new java.util.Scanner(is);
+        // read the response body to a string
+        String response = s.useDelimiter("\\A").next();
+        // close the scanner
+        s.close();	            
+        // return
+        return response;
+	}
+	
+	public static String getString(File file) throws IOException {
+		// prepare a scanner
+        Scanner s = new java.util.Scanner(file);
         // read the response body to a string
         String response = s.useDelimiter("\\A").next();
         // close the scanner
