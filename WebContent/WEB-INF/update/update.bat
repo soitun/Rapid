d3bbe24bd9a7332b64926623abf5b4c951a8e5f1
Gareth Edwards <gareth.edwards@rapid-is.co.uk>
2016-01-22 17:42:42 +0000

    Tiny change to update.bat and properties.js header

--- WebContent/WEB-INF/update/update.bat
+++ WebContent/WEB-INF/update/update.bat
@@ -1,2 +1,4 @@
 # run cmd.exe as admin and run update.jar - make sure the folder below is correct!
 %windir%\system32\cmd.exe /C "cd \Tomcat\webapps\rapid\WEB-INF\update & java -jar update.jar"
+
+#or make a shortcut for cmd.exe and make the above it's target with 
