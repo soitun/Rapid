b2ac7f7f4ee7e377d8ff163721a25555a7741a7b
Alexi Demetriou <alexi.demetriou@rapid-is.co.uk>
2016-01-29 11:11:07 +0000

    Sync

---
+++ src/com/rapid/core/Theme.java
@@ -0,0 +1,29 @@
+package com.rapid.core;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Theme  {
+	
+	// private instance variables
+	private String _type, _name, _css;
+	private JSONObject _resources;
+
+	// properties
+	public String getType() { return _type; }
+	public String getName() { return _name; }
+	public String getCSS() { return _css; }
+	public JSONObject getResources()  { return _resources; }
+
+	// constructor
+	public Theme(String xml) throws JSONException {		
+		// convert the xml string into JSON
+		JSONObject jsonTemplate = org.json.XML.toJSONObject(xml).getJSONObject("template");
+		// retain properties
+		_type = jsonTemplate.getString("type");
+		_name = jsonTemplate.getString("name");
+		_css = jsonTemplate.getString("css");
+		_resources = jsonTemplate.optJSONObject("resources");
+	}
+	
+}
