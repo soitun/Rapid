e0af60e2f0a07872d72034f017cc684369471ae6
Alexi Demetriou <alexi.demetriou@rapid-is.co.uk>
2016-08-31 12:18:03 +0100

    Rapid font library (Logo) and Email action for Rapid Admin.

---
+++ src/com/rapid/actions/Email.java
@@ -0,0 +1,117 @@
+/*
+
+Copyright (C) 2016 - Gareth Edwards / Rapid Information Systems
+
+gareth.edwards@rapid-is.co.uk
+
+
+This file is part of the Rapid Application Platform
+
+RapidSOA is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as 
+published by the Free Software Foundation, either version 3 of the 
+License, or (at your option) any later version. The terms require you 
+to include the original copyright, and the license notice in all redistributions.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+in a file named "COPYING".  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+package com.rapid.actions;
+
+import org.json.JSONObject;
+
+import com.rapid.core.Action;
+import com.rapid.core.Application;
+import com.rapid.core.Control;
+import com.rapid.core.Page;
+import com.rapid.server.RapidHttpServlet;
+import com.rapid.server.RapidRequest;
+
+public class Email extends Action {
+	
+	// parameterless constructor (required for jaxb)
+	public Email() { super(); }
+	// designer constructor
+	public Email(RapidHttpServlet rapidServlet, JSONObject jsonAction) throws Exception { 
+		super(rapidServlet, jsonAction);				
+	}
+	
+	@Override
+	public String getJavaScript(RapidRequest rapidRequest, Application application, Page page, Control control, JSONObject jsonDetails) throws Exception {
+		
+		// start with empty JavaScript
+        String js = "";
+        
+		// control can be null when the action is called from the page load
+        String controlParam = "";
+        if (control != null) controlParam = "&c=" + control.getId();
+        
+        // get the to control id
+        String toControlId = getProperty("to");
+        // get the to field
+        String toField = getProperty("toField");
+        
+        // assume empty data
+        js += "var data = {};\n";
+        
+        // get the get data js call for the to and to field
+        String getToJs = Control.getDataJavaScript(rapidRequest.getRapidServlet().getServletContext(), application, page, toControlId, toField);
+        
+        // check we got some
+        if (getToJs == null) {
+        	
+        	js = "// email to control can't be found\n";
+        	
+        } else {
+        	
+	        // add the to address
+	        js += "data.to = " + getToJs + ";\n";
+	        
+	        // get the body
+	        String jsonBody = getProperty("body");
+			
+			// open the ajax call
+	        js += "  $.ajax({ url : '~?a=" + application.getId() + "&v=" + application.getVersion() + "&p=" + page.getId() + controlParam + "&act=" + getId() + "', type: 'POST', contentType: 'application/json', dataType: 'json',\n";
+	        js += "    data: JSON.stringify(data),\n";
+	        js += "    error: function(server, status, fields, rows) {\n";
+	        // this avoids doing the errors if the page is unloading or the back button was pressed
+	        js += "      if (server.readyState > 0) {\n";
+	        // show the server exception message
+	        js += "        alert('Error with email action : ' + server.responseText||message);";	
+	        // close unloading check
+	        js += "      }\n";				       
+	        // close error actions
+	        js += "    }\n";
+	        // close ajax call
+	        js += "  });\n";
+        }
+
+		
+		return js;
+	}
+	
+	@Override
+    public JSONObject doAction(RapidRequest rapidRequest, JSONObject jsonData) throws Exception {
+		
+		// get the from address
+		String from = getProperty("from");
+		// get the subject
+		String subject = getProperty("subject");
+		
+		// get the to address
+		String to = jsonData.getString("to");
+		
+		com.rapid.core.Email.send(from, to, subject, "Yada yada...");
+		
+		// return an empty json object
+		return new JSONObject();
+	}
+	
+}
